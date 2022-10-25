66adfc36ce5b5f21e2a87ffc2a36b448dd509e3a
julien verlaguet <julien.verlaguet@gmail.com>
2022-10-25 06:57:12 +0000

    made makeFiles.sh more generic + added slides about type-inference among other things

--- makeFiles.sh
+++ makeFiles.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-for i in {1..100}; do
+mkdir -p $1
+
+for i in {1..1000}; do
     rm -f /tmp/test$i.msk
-    for j in {1..100}; do
-        echo "class Test${i}x${j}{a: Int}" >> /tmp/test$i.msk;
+    for j in {1..1000}; do
+        echo "class Test${i}x${j}{a: Int}" >> "$1/test$i.skml";
     done;
 done;
 
-echo "fun main() { Test42x42{a => 42 } }" >> /tmp/test1.msk
+echo "fun main() { Test42x42{a => 42 } }" >> "$1/test1.skml"
